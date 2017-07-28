1d4ac29a398f86afabb53c9be62891c9ba3f5a20
rbrtsmith <rbrtsmith84@googlemail.com>
2017-07-28 15:25:30 +0100

    Add hooks

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 const Redibox = require('redibox').default;
-const routes = require('./routes')
+const routes = require('./routes');
 
 const defaults = {
   prefix: '/redibox',
 };
 
 function api(app, config = defaults) {
-  const RediBox = new Redibox(config);
-  const { prefix } = config;
-
-  routes(app, prefix, RediBox);
+  global.RediBox = new Redibox(config, () => {
+    const { prefix } = config;
+    routes(app, prefix, RediBox);
+  });
 }
 
 module.exports = api;
--- src/routes.js
+++ src/routes.js
@@ -4,57 +4,85 @@ function routes(app, prefix, core) {
   const get = (path, cb) => app.get(`${prefix}${path}`, cb);
 
   get('/', (req, res) => {
+    const hooks = {};
+    const keys = Object.keys(core.hooks);
+
+    for (let i = 0; i < keys.length; i++) {
+      const hook = core.hooks[keys[i]];
+      hooks[keys[i]] = Object.assign({}, hook.options);
+    }
+
     res.json({
       prefix,
       id: core.id,
       options: core.options,
       uptimeSeconds: getUptime(core.bootedAtTimestamp),
-      hooks: Object.keys(core.hooks),
       clientsCount: core._clientCount,
       hostInfo: core.hostInfo(),
       scripts: core.scripts,
+      hooks,
     });
   });
 
-  get('/prefix', (req, res) => {
-    res.json(prefix);
-  });
-
-  get('/id', (req, res) => {
-    res.json(core.id);
-  });
-
-  get('/options', (req, res) => {
-    res.json(core.options);
-  });
-
-  get('/options/redis', (req, res) => {
-    res.json(core.options.redis);
-  });
-
-  get('/options/redis/hosts', (req, res) => {
-    res.json(core.options.redis.hosts);
-  });
-
-  get('/uptime', (req, res) => {
-    res.json(getUptime(core.bootedAtTimestamp));
-  });
+  // get('/prefix', (req, res) => {
+  //   res.json(prefix);
+  // });
+  //
+  // get('/id', (req, res) => {
+  //   res.json(core.id);
+  // });
+  //
+  // get('/options', (req, res) => {
+  //   res.json(core.options);
+  // });
+  //
+  // get('/options/redis', (req, res) => {
+  //   res.json(core.options.redis);
+  // });
+  //
+  // get('/options/redis/hosts', (req, res) => {
+  //   res.json(core.options.redis.hosts);
+  // });
+  //
+  // get('/uptime', (req, res) => {
+  //   res.json(getUptime(core.bootedAtTimestamp));
+  // });
 
   get('/hooks', (req, res) => {
     res.json(Object.keys(core.hooks));
   });
 
-  get('/clientscount', (req, res) => {
-    res.json(core._clientCount);
+  get('/hooks/:name', (req, res) => {
+    const name = req.param('name');
+    res.json(core.hooks[name].options);
   });
 
-  get('/hostinfo', (req, res) => {
-    res.json(core.hostInfo());
+  get('/hooks/:name/:method', (req, res) => {
+    const name = req.param('name');
+    const method = req.param('method');
+
+    if (core.hooks[name] && core.hooks[name][method] && typeof core.hooks[name][method] === 'function') {
+      core.hooks[name][method](/* todo args */).then(result => {
+        res.json(result)
+      }).catch(error => {
+        res.error(error);
+      });
+    } else {
+      // todo return 404
+    }
   });
 
-  get('/scripts', (req, res) => {
-    res.json(core.scripts);
-  });
+  // get('/clientscount', (req, res) => {
+  //   res.json(core._clientCount);
+  // });
+  //
+  // get('/hostinfo', (req, res) => {
+  //   res.json(core.hostInfo());
+  // });
+  //
+  // get('/scripts', (req, res) => {
+  //   res.json(core.scripts);
+  // });
 }
 
 module.exports = routes;
